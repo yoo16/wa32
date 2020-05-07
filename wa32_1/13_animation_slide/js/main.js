a7b88889bbf6fd039da370552668aabc90ec8d89
Yohei Yoshikawa <yohei.yoshikawa@gmail.com>
2020-05-07 10:27:35 +0900

    add & change samples

---
+++ wa32_1/13_animation_slide/js/main.js
@@ -0,0 +1,51 @@
+$(function () {
+    let photo = $('#photo');
+
+    //slide right
+    function slideToRight() {
+        photo.animate(
+            { 'left': 300 },
+            1000,
+            'linear',
+            function () {
+                setTimeout(slideToDown, 1000);
+            });
+    };
+
+    //slide left
+    function slideToLeft() {
+        photo.animate({ 'left': 0 }, 1000,
+            'linear',
+            function () {
+                setTimeout(slideToUp, 1000)
+            });
+    };
+
+    //slide down
+    function slideToDown() {
+        photo.animate({ 'top': 100 }, 1000,
+            'linear',
+            function () {
+                setTimeout(slideToLeftUp, 1000);
+            });
+    };
+
+    //slide left up
+    function slideToLeftUp() {
+        photo.animate({ 'left': 0, 'top': 0 }, 1000,
+            'linear',
+            function () {
+                setTimeout(slideToRight, 1000);
+            });
+    };
+
+    //slide up
+    function slideToUp() {
+        photo.animate({ 'top': 0 }, 1000,
+            'linear',
+            function () {
+                setTimeout(slideToLeft, 1000);
+            });
+    };
+    slideToRight();
+});
