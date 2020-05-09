daa30480ffec878b3af246298c21b15e96e379d6
Yohei Yoshikawa <yohei.yoshikawa@gmail.com>
2020-05-09 14:04:01 +0900

    change api samples

--- wa32_1/13_api/covid19/js/api.js
+++ wa32_1/13_api/covid19/js/api.js
@@ -2,26 +2,22 @@
 const prefecturesUrl = 'https://covid19-japan-web-api.now.sh/api/v1/prefectures';
 const positivesUrl = 'https://covid19-japan-web-api.now.sh/api/v1/positives';
 
-let prefectureList = $('#prefectureList');
-let positivesList = $('#positivesList');
-let currentPrefecture = '';
-
 $(function () {
-    /**
-     * click clear
-     */
-    $('#clear').on('click', function () {
-        $('.address').val('');
-    });
+    let prefectureList = $('#prefectureList');
+    let positivesList = $('#positivesList');
+    let loadingWindow = $('#loading');
+    let currentPrefecture = '';
 
     /**
      * click search
      */
     $('#search').on('click', function () {
+        showLoadingWindow();
         $.getJSON(prefecturesUrl, {}
         ).done(function (data) {
             showJson(data);
             showList(data);
+            hideLoadingWindow();
         });
     });
 
@@ -43,31 +39,27 @@ $(function () {
     function showList(values) {
         prefectureList.html('');
         //console.log(values);
-        $.each(values, function(key, value){
+
+        let title = $('<div class="col-12"><h2 class="h2">コロナの都道府県別データ</h2></div>');
+        prefectureList.append(title);
+
+        $.each(values, function (key, value) {
             let div = $('<div class="col-4"></div>');
-            let card = $('<div class=""></div>');
-            let cardHeader = $('<div class=""></div>');
-            let cardBody = $('<div class=""></div>');
 
-            let prefecture = $('<h2 class="h2"></div>');
-            prefecture.html(value.name_ja);
-            cardHeader.append(prefecture);
-
+            let prefecture = $('<h2 class="h2"></div>').html(value.name_ja);
             let pcr = $('<div></div>').html('PCR検査数: ' + value.pcr);
             let cases = $('<div></div>').html('発症者数: ' + value.cases);
             let deaths = $('<div></div>').html('死者数: ' + value.deaths);
-            let searchBtn = $('<button class="searchPositives btn btn-block btn-outline-primary mt-2">検索</button>');
+            let searchBtn = $('<button class="searchPositives btn btn-block btn-outline-primary mt-2">詳細</button>');
             let prefectureValue = prefectures[value.id];
             searchBtn.attr('prefecture', prefectureValue.label);
 
-            cardBody.append(pcr);
-            cardBody.append(cases);
-            cardBody.append(deaths);
-            cardBody.append(searchBtn);
+            div.append(prefecture);
+            div.append(pcr);
+            div.append(cases);
+            div.append(deaths);
+            div.append(searchBtn);
 
-            card.append(cardHeader);
-            card.append(cardBody);
-            div.append(card);
             prefectureList.append(div);
         });
 
@@ -77,14 +69,32 @@ $(function () {
         $('.searchPositives').off('click');
         $('.searchPositives').on('click', function () {
             //console.log($(this).attr('prefecture'));
+            showLoadingWindow();
             currentPrefecture = $(this).attr('prefecture');
             $.getJSON(positivesUrl, { prefecture: currentPrefecture }
             ).done(function (data) {
                 //console.log(data);
                 showPositives(data);
                 showJson(data);
+                hideLoadingWindow();
             });
         });
+    }
+
+    /**
+     * show loading
+     */
+    function showLoadingWindow() {
+        console.log('showLoading');
+        console.log(loadingWindow);
+        loadingWindow.show();
+    }
+
+    /**
+     * hide loading
+     */
+    function hideLoadingWindow() {
+        loadingWindow.hide();
     }
 
     /**
@@ -95,42 +105,38 @@ $(function () {
     function showPositives(values) {
         positivesList.html('');
         //console.log(values);
-        $.each(values, function(key, value){
+        let title = $('<div class="col-12"></div>');
+        let h2 = $('<h2 class="h2"></h2>');
+        h2.html(currentPrefecture);
+        title.append(h2);
+        positivesList.append(title);
+        $.each(values, function (key, value) {
             let div = $('<div class="col-4"></div>');
-            let card = $('<div class=""></div>');
-            let cardHeader = $('<div class=""></div>');
-            let cardBody = $('<div class=""></div>');
-
-            let prefecture = $('<h2 class="h2"></div>');
-            prefecture.html(value.name_ja);
-            cardHeader.append(prefecture);
-
+            let residence_prefecture = $('<h2 class="h2"></div>').html(value.residence_prefecture);
             let diagnosis_date = $('<div></div>').html('発症日: ' + value.diagnosis_date);
             let age = $('<div></div>').html('年代: ' + value.age);
             let gender = $('<div></div>').html('性別: ' + value.gender);
-            let residence_prefecture = $('<div></div>').html('市区町村: ' + value.residence_prefecture);
 
-            cardBody.append(diagnosis_date);
-            cardBody.append(age);
-            cardBody.append(gender);
-            cardBody.append(residence_prefecture);
+            div.append(residence_prefecture);
+            div.append(diagnosis_date);
+            div.append(age);
+            div.append(gender);
 
-            card.append(cardHeader);
-            card.append(cardBody);
-            div.append(card);
             positivesList.append(div);
         });
 
-        let closeBtn = $('<button id="closePositives" class="btn btn-block btn-outline-primary mt-2">閉じる</button>');
+        let closeBtn = $('<button id="closePositives" class="btn btn-block btn-outline-primary mt-2">戻る</button>');
         $(closeBtn).off('click');
         $(closeBtn).on('click', function () {
-            positivesList.hide();
-            prefectureList.show();
+            // positivesList.hide();
+            // prefectureList.show();
+            scrollTo(prefectureList);
         });
 
         positivesList.append(closeBtn);
-        positivesList.show();
-        prefectureList.hide();
+        // positivesList.show();
+        // prefectureList.hide();
+        scrollTo(positivesList);
     }
 
     /**
@@ -143,6 +149,20 @@ $(function () {
         });
     }
 
+    function scrollTo(target) {
+        var time = 400;
+        var offsetY = -50;
+
+        $('html, body').animate({
+            scrollTop: $(target).offset().top + offsetY
+        }, {
+            duration: time,
+            easing: 'easeOutExpo'
+        });
+        return false;
+    }
+
     loadPrefecture()
-    positivesList.hide();
+    //positivesList.hide();
+
 });
