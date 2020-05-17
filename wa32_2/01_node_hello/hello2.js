0662c58f104b0e5ab83a202db21d492f334c1db1
Yohei Yoshikawa <yohei.yoshikawa@gmail.com>
2020-05-17 15:53:24 +0900

    add css sample update homework 03 change css document ready wa32_2

---
+++ wa32_2/01_node_hello/hello2.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let name = "HAL";
+console.log(`Hello 
+${name}`); // テンプレートリテラル
+console.log("Hello %s", name);
+console.log("Hello " + name);
---
+++ wa32_2/01_node_hello/save.js
@@ -0,0 +1,9 @@
+"use strict";
+let messages = [
+    "こんにちは",
+    "おはよう",
+    "サヨナラ"
+];
+
+console.log(messages);
+messages.forEach(message => console.log(message));
