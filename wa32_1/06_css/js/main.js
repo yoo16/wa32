6e368b89531615ea6c4b2c2969a42bb8eb723e05
Yohei Yoshikawa <yohei.yoshikawa@gmail.com>
2020-05-26 11:15:11 +0900

    update sample 06 css

--- wa32_1/06_css/js/main.js
+++ wa32_1/06_css/js/main.js
@@ -1,42 +1,26 @@
 $(function() {
-    $('#txt1').css('color', '#090000');
-
-    $('#txt2').css('font-size', '24px');
-    $('#txt3').css('fontSize', '2.0rem');
-
-    $('#txt1').css('font-weight', 'bold');
-
-    $('#txt4').css({
-        'color': 'red',
-        'font-size': '32px',
-        'font-weight': 'bold'
-    });
+    /**
+     * css()
+     */
+    $('input[name=item]').css('background', '#ff0000');
+    // $('input[name=item]').css('background', 'rgb(255, 0, 0)');
+    // $('input[name=item]').css('background', 'rgba(255, 0, 0, 1.0)');
 
     let color = $('#txt4').css('color');
-    
-    $('#txt1').css('color', color);
-    $('#box1').width('40px');
-    $('#box2').width('70px').height('100px');
+    $('#color_result').html(color);
 
-    console.log('width:' + $('#box1').width());
-    console.log('height:' + $('#box2').height());
+    /**
+     * box1
+     */
+    // console.log($('#box1'));
+    let width = $('#box1').width();
+    let height = $('#box1').height();
+    let result = `width: ${Math.round(width)} height: ${Math.round(height)}`;
+    $('#window_size_result').html(result);
 
-    $('#txt5').css({
-        'color': 'black',
-        'font-size': '48px',
-        'font-weight': 'bold',
-        'border': 'solid',
-        'text-align': 'center',
-        'background-color': 'yellow'
-    });
-
-    $('#txt6').css({
-        'color': 'red',
-        'font-size': '48px',
-        'font-weight': 'bold',
-        'border': 'solid',
-        'text-align': 'center',
-        'background': 'repeating-linear-gradient(-30deg, black 0, black 10px, yellow 10px, yellow 20px)'
-    });
-
+    /**
+     * box2
+     */
+    $('#box2').width('150px');
+    $('#box2').height('100px');
 });
