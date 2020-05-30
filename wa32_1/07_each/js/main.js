91a13f4b520dc01a64cb90c777c851d70ac68516
Yohei Yoshikawa <yohei.yoshikawa@gmail.com>
2020-05-31 01:55:35 +0900

    update ready event

--- wa32_1/07_each/js/main.js
+++ wa32_1/07_each/js/main.js
@@ -1,4 +1,12 @@
 $(function () {
+    $('#city li').each(function(index, element){
+        console.log(index);
+        console.log(element);
+        console.log(this);
+        console.log($(this).text());
+    });
+
+
     let items = [
         { index:0, name: '銅のつるぎ', price: 150, amount: 1},
         { index:1, name: '鉄のやり', price: 550, amount: 1},
@@ -7,14 +15,11 @@ $(function () {
         { index:4, name: '皮のぼうし', price: 50, amount: 1},
     ];
 
-    $.each(items, function(key, value) {
-        console.log(key);
-        console.log(value);
-    });
-
-    $('li').each(function(index, element){
+    $.each(items, function(index, value) {
         console.log(index);
-        console.log(element);
+        console.log(value);
+        console.log(value.name);
+        console.log(this.name);
     });
 
 });
--- wa32_1/07_ready/js/main.js
+++ wa32_1/07_ready/js/main.js
@@ -1,13 +1,19 @@
-$('p').css('color', '#f00');
+$(window).on('load', function(){
+    console.log('window load');
+});
+
+$(document).on('load', function(){
+    console.log('document load');
+});
 
 $(function() {
-
+    console.log('$');
 });
 
 $(document).ready(function () {
+    console.log('ready()');
+});
 
-})
-
-$(document).on('ready', function () {
-
-})
+$(document).on('ready', function() {
+    console.log('on ready');
+});
