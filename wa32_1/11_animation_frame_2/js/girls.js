03e1c0e066ff00fd736468ecb280047dd65ba561
Yohei Yoshikawa <yohei.yoshikawa@gmail.com>
2020-06-18 10:43:52 +0900

    update frame samples

--- wa32_1/11_animation_frame_2/js/girls.js
+++ wa32_1/11_animation_frame_2/js/girls.js
@@ -1,9 +1,9 @@
 $(function () {
-    let girl = $('#girl');
-    let intervalID;
-    let duration = 150;
-    let col = 4;
-    let max = 8;
+    let girl = $('#girl')
+    let intervalID
+    let duration = 150
+    let col = 4
+    let max = 8
 
     girl.prop({
         w: $('#girl-box').width(),
@@ -11,39 +11,59 @@ $(function () {
         col: col,
         max: max,
         num: 0 
-    });
+    })
+
+    $('#change_pic').on('click', function() {
+        let number = Math.ceil(Math.random() * max) - 1
+        console.log(number)
+
+        let colCount = girl.prop('col')
+        let col = number % colCount
+        let row = Math.floor(number / colCount)
+        let width = girl.prop('w')
+        let height = girl.prop('h')
+        let x = width * col
+        let y = height * row
+
+        girl.css({
+            'top': -y + 'px',
+            'left': -x + 'px'
+        })
+    })
 
     /**
      * change
      */
     function change(target) {
-        let currentNo = target.prop('num');
-        let nextNo = currentNo + 1;
-        let maxNo = target.prop('max');
-        let width = target.prop('w');
-        let height = target.prop('h');
-        let colCount = target.prop('col');
-        let col = currentNo % colCount;
-        let row = Math.floor(currentNo / colCount);
-        let x = width * col;
-        let y = height * row;
+        let currentNo = target.prop('num')
+        let nextNo = currentNo + 1
+        let maxNo = target.prop('max')
+        let width = target.prop('w')
+        let height = target.prop('h')
+        let colCount = target.prop('col')
+        let col = currentNo % colCount
+        let row = Math.floor(currentNo / colCount)
+        let x = width * col
+        let y = height * row
 
-        target.prop('num', nextNo);
+        target.prop('num', nextNo)
         if (nextNo > maxNo) {
-            clearInterval(intervalID);
+            clearInterval(intervalID)
         } else {
             target.css({
                 'top': -y + 'px',
                 'left': -x + 'px'
-            });
+            })
         }
-    };
+    }
 
     /**
      * loaded
      */
     function loaded() {
-        intervalID = setInterval(function () { change(girl); }, duration);
+        intervalID = setInterval(function () { 
+            change(girl)
+        }, duration)
     }
 
     /**
@@ -51,8 +71,8 @@ $(function () {
      */
     function init() {
         let img = new Image();
-        $(img).on('load', loaded).attr('src', girl.attr('src'));
-    };
+        $(img).attr('src', girl.attr('src')).on('load', loaded)
+    }
 
-    init();
-});
+    init()
+})
